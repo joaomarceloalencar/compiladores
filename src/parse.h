8f42faf522a85cdf514eee0b7bc804b3da0d7d73
jmhal <jmarcelo.alencar@gmail.com>
2018-04-12 18:32:03 -0300

    Cabeçalho do Analisador Sintático.

---
+++ src/parse.h
@@ -0,0 +1,15 @@
+/************************************************************/
+/* Arquivo: parse.h                                         */
+/* Interface do analisador sintático para o compilador TINY */
+/* Compiladores: princípios e práticas                      */
+/* Kenneth C. Louden                                        */
+/************************************************************/
+
+#ifndef _PARSE_H_
+#define _PARSE_H_
+
+/* Função parse retorna a árvore sintática nova construída */
+
+TreeNode *parse(void);
+
+#endif
