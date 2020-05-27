9d5cd9c026a744d4a7b96a6f71f184879cdac5ea
Joao Marcelo <joao.marcelo@ufc.br>
2020-05-27 07:56:49 -0300

    Exemplo para demonstração da pilha de execução.

---
+++ src/ambientes/pilhas.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int x = 2;
+
+void g(int);
+
+void f(int n) {
+    static int x = 1;
+    g(n);
+    x--;
+}
+
+void g(int m) {
+    int y = m - 1;
+    if (y > 0) {
+        f(y);
+        x--;
+        g(y);
+    }
+}
+
+int main() {
+    g(x);
+    return 0;
+}
